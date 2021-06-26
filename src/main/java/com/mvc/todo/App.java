4c43e0d5111f550f1b44e61fcf6c7462056bb243
sriman <sriman456.singh@gmail.com>
2021-06-26 17:40:51 +0800

    Initial commit

---
+++ src/main/java/com/mvc/todo/App.java
@@ -0,0 +1,13 @@
+package com.mvc.todo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
